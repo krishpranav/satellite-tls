26d1c2f09b4de151bf68721aae215d97d219226b
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-14 13:42:45 +0530

    src: constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,21 @@
 pub use std::f64::consts::PI;
 
 pub static TWO_PI: f64 = PI * 2.0;
+
+pub static DEG_2_RAD: f64 = PI / 180.0;
+
+pub static RAD_TO_DEG: f64 = 180.0 / PI;
+
+pub static MINUTES_PER_DAY: f64 = 1440.0;
+
+pub static MU: f64 = 398600.5; 
+
+pub static EARTH_RADIUS: f64 = 6378.137;
+
+pub static X2O3: f64 = 2.0 / 3.0;
+
+pub static C: f64 = 299792.458;
+
+pub static KM_TO_MI: f64 = 0.62137;
+
+pub static MI_TO_KM: f64 = 1.0 / 0.62137;
